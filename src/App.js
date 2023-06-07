3a752f59231161fd43181eaef3ef3db8eed464a0
gabrielrowan <gabrielrowan82@gmail.com>
2023-06-07 16:20:36 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -24,11 +24,10 @@ function reducer(state, {type, payload})
         currentOperand: null
       }
     }
-    if (state.currentOperand == null) return state
+    if (state.currentOperand == null) return state  
     if (state.currentOperand.length === 1) {
       return {...state, currentOperand: null }
     }
-
     return {
       ...state,
       currentOperand: state.currentOperand.slice(0, -1)
@@ -42,7 +41,6 @@ function reducer(state, {type, payload})
             overwrite: false
           }
         }
-
         if (payload.digit === "0" && state.currentOperand == "0") return state
         if (payload.digit === "." && state.currentOperand.includes(".")) return state
         return {
@@ -50,11 +48,11 @@ function reducer(state, {type, payload})
           currentOperand: `${state.currentOperand || ""}${payload.digit} `,
         }
 
-        case ACTIONS.CHOOSE_OPERATION:
-          if (state.currentOperand == null && state.previousOperand == null)
-          {
+      case ACTIONS.CHOOSE_OPERATION:
+        if (state.currentOperand == null && state.previousOperand == null)
+        {
             return state
-          }
+        }
         if (state.previousOperand == null)
         {
           return {
@@ -70,7 +68,6 @@ function reducer(state, {type, payload})
             operation: payload.operation
           }
         }
-
         return {
           ...state,
           previousOperand: evaluate(state),
@@ -78,35 +75,37 @@ function reducer(state, {type, payload})
           currentOperand: null
         }
 
-        case ACTIONS.CLEAR:
+      case ACTIONS.CLEAR:
           return {}
           break;
 
-        case ACTIONS.EVALUATE:
-          if 
-          (state.operation == null ||
-            state.currentOperand == null ||
-            state.previousOperand == null)
-            {
-              return state
-            }
-
-          return {
-            ...state,
-            previousOperand: null,
-            overwrite: true,
-            operation: null,
-            currentOperand: evaluate(state)
+      case ACTIONS.EVALUATE:
+        if 
+        (state.operation == null ||
+          state.currentOperand == null ||
+          state.previousOperand == null)
+          {
+            return state
           }
 
+        return {
+          ...state,
+          previousOperand: null,
+          overwrite: true,
+          operation: null,
+          currentOperand: evaluate(state)
+        }
   }
 }
 
 function evaluate ({currentOperand, previousOperand, operation}){
   const previous = parseFloat(previousOperand)
   const current = parseFloat(currentOperand)
+
   if (isNaN(previous) || isNaN(current)) return ""
+
   let computation = ""
+
   switch (operation) {
     case "+":
       computation = previous + current;
@@ -139,7 +138,7 @@ function App() {
         <div className="previous-operand">
           {previousOperand} {operation}
         </div>
-        <div className="current-operand">{currentOperand == null? " " : currentOperand}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two" onClick={() => dispatch({type: ACTIONS.CLEAR})}>AC</button>
       <button onClick={() => dispatch({type: ACTIONS.DELETE_DIGIT})}>DEL</button>
